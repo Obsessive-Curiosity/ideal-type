eb0bb784770ddcc8b607852235c3016d89c4ba7d
MIN JI-YEONG <kuck523@gmail.com>
2024-09-11 19:22:08 +0900

    Update Result.tsx

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useRef, useState } from "react";
-import { toPng } from "html-to-image";
+import { toPng, toBlob } from "html-to-image";
 import styled from "styled-components";
 import Header from "../components/Header/index";
 import ResultItem from "../components/ResultItem/index";
@@ -8,20 +8,17 @@ const Result = () => {
   const meRef = useRef<HTMLDivElement>(null);
   const youRef = useRef<HTMLDivElement>(null);
   const [combinedImageSrc, setCombinedImageSrc] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true); // 로딩 상태를 추적
+  const [isLoading, setIsLoading] = useState(true);
 
   useEffect(() => {
     const generateImages = async () => {
       if (meRef.current && youRef.current) {
         try {
-          // 폰트가 로드될 때까지 기다립니다.
           await document.fonts.ready;
 
-          // 각 이미지를 캡처합니다.
-          const meDataUrl = await toPng(meRef.current, {});
-          const youDataUrl = await toPng(youRef.current, {});
+          const meBlob = await toBlob(meRef.current);
+          const youBlob = await toBlob(youRef.current);
 
-          // 캔버스를 사용하여 이미지를 결합합니다.
           const canvas = document.createElement("canvas");
           const ctx = canvas.getContext("2d");
 
@@ -29,23 +26,23 @@ const Result = () => {
             throw new Error("Canvas context is not available.");
           }
 
-          // 이미지 로드가 완료되면 캔버스에 그립니다.
-          const loadImage = (src: string) => {
+          const meImage = new Image();
+          const youImage = new Image();
+
+          const loadImage = (blob: Blob) => {
             return new Promise<HTMLImageElement>((resolve, reject) => {
               const image = new Image();
               image.onload = () => resolve(image);
               image.onerror = reject;
-              image.src = src;
+              image.src = URL.createObjectURL(blob);
             });
           };
 
-          // 이미지 로드
           const [meImageLoaded, youImageLoaded] = await Promise.all([
-            loadImage(meDataUrl),
-            loadImage(youDataUrl),
+            loadImage(meBlob),
+            loadImage(youBlob),
           ]);
 
-          // 캔버스 크기를 설정합니다. 두 이미지를 나란히 배치합니다.
           const devicePixelRatio = window.devicePixelRatio || 1;
           canvas.width =
             (meImageLoaded.width + youImageLoaded.width) * devicePixelRatio;
@@ -54,21 +51,21 @@ const Result = () => {
             devicePixelRatio;
           ctx.scale(devicePixelRatio, devicePixelRatio);
 
-          // 두 이미지를 캔버스에 그립니다.
           ctx.drawImage(meImageLoaded, 0, 0);
           ctx.drawImage(youImageLoaded, meImageLoaded.width, 0);
 
-          // 결합된 이미지의 Data URL을 가져옵니다.
-          const combinedDataUrl = canvas.toDataURL("image/png");
-          setCombinedImageSrc(combinedDataUrl);
-          setIsLoading(false); // 로딩 완료 후 상태 변경
+          const combinedBlob = await new Promise<Blob>((resolve) =>
+            canvas.toBlob(resolve, "image/png")
+          );
+          const url = URL.createObjectURL(combinedBlob);
+          setCombinedImageSrc(url);
+          setIsLoading(false);
         } catch (err) {
           console.error("이미지 생성 중 오류가 발생했습니다.", err);
         }
       }
     };
 
-    // 요청 애니메이션 프레임을 통해 이미지 생성
     requestAnimationFrame(() => {
       requestAnimationFrame(generateImages);
     });
@@ -105,10 +102,10 @@ const ImageWrapper = styled.div`
   img {
     max-width: 100%;
     height: auto;
+    width: 100%;
   }
 `;
 
-// ResultItem을 visibility와 position 속성으로 숨기고 보이게 합니다.
 const ResultItemContainer = styled.div<{ isLoading: boolean }>`
   h4 {
     text-align: center;
