a6fbdec2e20d304b80e4dc366069cd9b52056936
MIN JI-YEONG <kuck523@gmail.com>
2024-09-11 19:51:44 +0900

    이미지 저장 수정

--- src/pages/Result.tsx
+++ src/pages/Result.tsx
@@ -1,99 +1,35 @@
-import { useEffect, useRef, useState } from "react";
-import { toBlob } from "html-to-image";
 import styled from "styled-components";
+import html2canvas from "html2canvas";
+import saveAs from "file-saver";
+import { useRef } from "react";
 import Header from "../components/Header/index";
 import ResultItem from "../components/ResultItem/index";
 
 const Result = () => {
   const meRef = useRef<HTMLDivElement>(null);
   const youRef = useRef<HTMLDivElement>(null);
-  const [combinedImageSrc, setCombinedImageSrc] = useState<string | null>(null);
-  const [isLoading, setIsLoading] = useState(true);
 
-  useEffect(() => {
-    const generateImages = async () => {
-      if (meRef.current && youRef.current) {
-        try {
-          await document.fonts.ready;
+  const handleDownload = async () => {
+    if (!meRef.current) return;
 
-          // Blob 형태로 이미지를 가져옵니다.
-          const meBlob = await toBlob(meRef.current);
-          const youBlob = await toBlob(youRef.current);
-
-          if (!meBlob || !youBlob) {
-            throw new Error("Failed to generate image blobs.");
-          }
-
-          const canvas = document.createElement("canvas");
-          const ctx = canvas.getContext("2d");
-
-          if (!ctx) {
-            throw new Error("Canvas context is not available.");
-          }
-
-          const meImage = new Image();
-          const youImage = new Image();
-
-          const loadImage = (blob: Blob) => {
-            return new Promise<HTMLImageElement>((resolve, reject) => {
-              const image = new Image();
-              image.onload = () => resolve(image);
-              image.onerror = reject;
-              image.src = URL.createObjectURL(blob);
-            });
-          };
-
-          const [meImageLoaded, youImageLoaded] = await Promise.all([
-            loadImage(meBlob),
-            loadImage(youBlob),
-          ]);
-
-          const devicePixelRatio = window.devicePixelRatio || 1;
-          canvas.width =
-            (meImageLoaded.width + youImageLoaded.width) * devicePixelRatio;
-          canvas.height =
-            Math.max(meImageLoaded.height, youImageLoaded.height) *
-            devicePixelRatio;
-          ctx.scale(devicePixelRatio, devicePixelRatio);
-
-          ctx.drawImage(meImageLoaded, 0, 0);
-          ctx.drawImage(youImageLoaded, meImageLoaded.width, 0);
-
-          // Blob을 사용하여 이미지 URL을 생성합니다.
-          const combinedBlob = await new Promise<Blob | null>((resolve) =>
-            canvas.toBlob(resolve, "image/png")
-          );
-
-          if (combinedBlob) {
-            const url = URL.createObjectURL(combinedBlob);
-            setCombinedImageSrc(url);
-          }
-
-          setIsLoading(false);
-        } catch (err) {
-          console.error("이미지 생성 중 오류가 발생했습니다.", err);
+    try {
+      const div = meRef.current;
+      const canvas = await html2canvas(div, { scale: 2 });
+      canvas.toBlob((blob) => {
+        if (blob !== null) {
+          saveAs(blob, "result.png");
         }
-      }
-    };
-
-    requestAnimationFrame(() => {
-      requestAnimationFrame(generateImages);
-    });
-  }, []);
+      });
+    } catch (error) {
+      console.error("Error converting div to image:", error);
+    }
+  };
 
   return (
     <Container>
       <Header title={"결과 보기"} />
-      <ResultItemContainer isLoading={isLoading}>
-        <h4>이미지 로딩중 입니다.</h4>
-        <ResultItem meRef={meRef} youRef={youRef} />
-      </ResultItemContainer>
-      {combinedImageSrc && (
-        <ImageWrapper>
-          <h4>이미지를 다운로드 하실 수 있습니다.</h4>
-          <img src={combinedImageSrc} alt="Combined Result" />
-        </ImageWrapper>
-      )}
+      <ResultItem meRef={meRef} youRef={youRef} />
+      <button onClick={handleDownload}>다운로드</button>{" "}
     </Container>
   );
 };
@@ -104,25 +40,4 @@ const Container = styled.div`
   min-height: 100vh;
   display: flex;
   flex-direction: column;
-`;
-
-const ImageWrapper = styled.div`
-  margin: 20px;
-  text-align: center;
-  img {
-    max-width: 100%;
-    height: auto;
-    width: 100%;
-  }
-`;
-
-const ResultItemContainer = styled.div<{ isLoading: boolean }>`
-  h4 {
-    text-align: center;
-    margin-top: 20px;
-  }
-  visibility: ${(props) => (props.isLoading ? "visible" : "hidden")};
-  position: ${(props) => (props.isLoading ? "relative" : "absolute")};
-  opacity: ${(props) => (props.isLoading ? 1 : 0)};
-  transition: opacity 0.5s ease;
 `;
