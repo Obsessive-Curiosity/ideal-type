71134d0af0a5d8c6f4307fb402e7c1e4aea2bff7
MIN JI-YEONG <kuck523@gmail.com>
2024-09-14 22:51:01 +0900

    Update index.tsx

--- src/components/Question/Personality/Mbti/index.tsx
+++ src/components/Question/Personality/Mbti/index.tsx
@@ -1,52 +1,59 @@
 import styled from "styled-components";
-import { useCallback, useEffect, useState } from "react";
+import { useEffect } from "react";
 import QuestionWrapper from "../../../../styles/QuestionWrapper";
 import QuesiotnProps from "../../../../interfaces/QuestionProps";
+import OPT_CHECKBOX from "../../../../constants/OPT_CHECKBOX";
+import useCheckbox from "../../../../hooks/useCheckbox";
+import CheckboxItem from "../../../QuestionItems/CheckboxItem";
 
-// MBTI 옵션 배열 정의
-const MBTI_OPTIONS = [
-  ["I", "E"],
-  ["N", "S"],
-  ["F", "T"],
-  ["P", "J"],
+const mbtiList: [string, typeof OPT_CHECKBOX.MULTI][] = [
+  ["ISTJ 세상의 소금형", OPT_CHECKBOX.MULTI],
+  ["ISFJ 임금뒷편의 권력형", OPT_CHECKBOX.MULTI],
+  ["INFJ 예언자형", OPT_CHECKBOX.MULTI],
+  ["INTJ 과학자형", OPT_CHECKBOX.MULTI],
+  ["ISTP 백과사전형", OPT_CHECKBOX.MULTI],
+  ["ISFP 성인군자형", OPT_CHECKBOX.MULTI],
+  ["INFP 잔다르크형", OPT_CHECKBOX.MULTI],
+  ["INTP 아이디어 뱅크형", OPT_CHECKBOX.MULTI],
+  ["ESTP 수완좋은 활동가형", OPT_CHECKBOX.MULTI],
+  ["ESFP 사교적인 유형", OPT_CHECKBOX.MULTI],
+  ["ENFP 스파크형", OPT_CHECKBOX.MULTI],
+  ["ENTP 발명가형", OPT_CHECKBOX.MULTI],
+  ["ESTJ 사업가형", OPT_CHECKBOX.MULTI],
+  ["ESFJ 친선도모형", OPT_CHECKBOX.MULTI],
+  ["ENFJ 언변능숙형", OPT_CHECKBOX.MULTI],
+  ["ENTJ 지도자형", OPT_CHECKBOX.MULTI],
 ];
 
 function Mbti({ id, setHandler }: QuesiotnProps) {
   const user = id === "1" ? "본인" : "상대방";
-  const [mbti, setMbti] = useState<string[]>(["I", "N", "F", "P"]);
+  const { selectedItems, onChangeCheckbox } = useCheckbox([]);
 
-  // MBTI 항목 클릭 시 선택 변경
-  const onClickMbti = useCallback((index: number) => {
-    setMbti((prevMbti) => {
-      const newMbti = [...prevMbti];
-      newMbti[index] = MBTI_OPTIONS[index].find(
-        (option) => option !== prevMbti[index]
-      )!;
-      return newMbti;
-    });
-  }, []);
-
-  // MBTI 변경 시 handler에 새로운 값 전달
   useEffect(() => {
-    setHandler("mbti", [mbti.join("")]);
-  }, [mbti, setHandler]);
+    setHandler("age", selectedItems);
+  }, [selectedItems, setHandler]);
 
   return (
     <QuestionWrapper>
       <h2>{user}의 MBTI를 선택해주세요.</h2>
+      <p>💡중복 선택 가능</p>
       <p>
         <LinkToMbti
           href="https://www.16personalities.com/ko"
           target="_blank"
           rel="noopener noreferrer"
         >
-          MBTI 검사하러 가기
+          💡MBTI 검사하러 가기
         </LinkToMbti>
       </p>
-      {mbti.map((item, index) => (
-        <MbtiBtn key={index} onClick={() => onClickMbti(index)}>
-          {item}
-        </MbtiBtn>
+      {mbtiList.map(([item, type], idx) => (
+        <CheckboxItem
+          key={idx}
+          item={item}
+          type={type}
+          isChecked={selectedItems.includes(item)}
+          onChange={() => onChangeCheckbox(item, type)}
+        />
       ))}
     </QuestionWrapper>
   );
@@ -54,28 +61,11 @@ function Mbti({ id, setHandler }: QuesiotnProps) {
 
 export default Mbti;
 
-// 스타일링
-const MbtiBtn = styled.button`
-  font-size: 30px;
-  width: 15vw;
-  max-width: 100px;
-  height: 15vw;
-  max-height: 100px;
-  margin: 0 1vw;
-  background-color: white;
-  border: 1px solid black;
-  border-radius: 10px;
-  &:hover {
-    cursor: pointer;
-    color: #694e99;
-  }
-`;
-
 const LinkToMbti = styled.a`
   font-style: italic;
-  color: #d5c9e6;
-  text-decoration: none;
+  color: #694e99;
   &:hover {
+    background-color: #d5c9e6;
     color: #694e99;
     text-decoration: wavy;
   }
