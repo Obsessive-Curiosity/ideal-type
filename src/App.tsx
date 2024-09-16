ea02ed576a91fda87c0d5603574af3733fcc7fbb
MIN JI-YEONG <kuck523@gmail.com>
2024-09-17 07:11:25 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -120,7 +120,7 @@ const GlobalStyle = createGlobalStyle`
     width:100%;
     max-width: 600px;
     margin: 0 auto;
-    min-height: 100svh;
+    height: auto;
     box-shadow: rgb(100,100,100, 0.2) 0px 0px 29px 0px;
     padding: 0px 20px;
   }
