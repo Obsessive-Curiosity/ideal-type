176d22df7e7a3eeb3a468d992c4cece4643de0d7
MIN JI-YEONG <kuck523@gmail.com>
2024-09-17 07:04:20 +0900

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -111,7 +111,7 @@ const GlobalStyle = createGlobalStyle`
   html, body {
     width: 100%;
     height: 100%;
-    overflow: hidden;
+    /* overflow: hidden; */
     background-color: rgb(246,246,246);
   }
 
