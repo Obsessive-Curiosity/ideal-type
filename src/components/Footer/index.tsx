9ab213039b925579b8f23da92ebb30d45efe2e25
MIN JI-YEONG <kuck523@gmail.com>
2024-09-17 20:04:50 +0900

    Update index.tsx

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -17,7 +17,6 @@ const index = ({ title, onClick }: FooterProps) => {
 export default index;
 
 const FooterWrapper = styled.footer`
-  display: flex;
   align-items: center;
   padding: 20px 0;
   border-top: 1px solid #d5c9e6;
